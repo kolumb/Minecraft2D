25f7e18d7e3d97b42723ef15cbbd9108169b887f
Danil Kolumbet <kolumbetko@gmail.com>
2021-03-27 01:35:27 +0200

    Initial commit with boilerplate

---
+++ utils.js
@@ -0,0 +1,37 @@
+"use strict";
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
+function clamp(n, limit) {
+    let lim = Math.abs(limit);
+    if (n < 0) {
+        if (Math.abs(n) > lim) {
+            return -lim;
+        }
+    } else {
+        if (Math.abs(n) > lim) {
+            return lim;
+        }
+    }
+    return n;
+}
+
+const distPointToLine = (p, l1, l2) =>
+    Math.abs((l2.x - l1.x) * (l1.y - p.y) - (l1.x - p.x) * (l2.y - l1.y))
+    / Math.sqrt((l2.x - l1.x)**2 + (l2.y - l1.y)**2);
+
+const determinant = (p, l1, l2) => (l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x)
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
